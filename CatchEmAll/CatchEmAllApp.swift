94feada06a3f5f58c0601b9c1900f6295e4a3043
nbmcguire <57973784+nbmcguire@users.noreply.github.com>
2025-03-17 14:21:46 -0400

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by Noah McGuire on 3/17/25.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
