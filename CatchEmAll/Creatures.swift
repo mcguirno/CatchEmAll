ca322d994616efbb9e91337179ac05782ab7e8aa
nbmcguire <57973784+nbmcguire@users.noreply.github.com>
2025-03-18 00:00:22 -0400

    Changes from 6.6

--- CatchEmAll/Creatures.swift
+++ CatchEmAll/Creatures.swift
@@ -11,7 +11,7 @@ import Foundation
 class Creatures {
     private struct Returned: Codable {
         var count: Int
-        var next: String
+        var next: String?
         var results: [Creature]
     }
     var urlString = "https://pokeapi.co/api/v2/pokemon/"
@@ -29,9 +29,11 @@ class Creatures {
                 print("ERROR: Could not decode returned JSON data")
                 return
             }
-            self.count = returned.count
-            self.urlString = returned.next
-            self.creaturesArray = returned.results
+            Task { @MainActor in
+                self.count = returned.count
+                self.urlString = returned.next ?? ""
+                self.creaturesArray = self.creaturesArray + returned.results
+            }
         } catch {
             print("ERROR: Could not get data from \(urlString)")
         }
--- CatchEmAll/CreaturesListView.swift
+++ CatchEmAll/CreaturesListView.swift
@@ -11,16 +11,29 @@ struct CreaturesListView: View {
     @State var creatures = Creatures()
     var body: some View {
         NavigationStack {
-            List(creatures.creaturesArray, id: \.self) { creature in
-                NavigationLink {
-                    DetailView(creature: creature)
-                } label: {
-                    Text(creature.name.capitalized)
-                        .font(.title2)
+            List(0..<creatures.creaturesArray.count, id: \.self) { index in
+                LazyVStack {
+                    NavigationLink {
+                        DetailView(creature: creatures.creaturesArray[index])
+                    } label: {
+                        Text("\(index+1). \(creatures.creaturesArray[index].name.capitalized)")
+                            .font(.title2)
+                    }
+                }
+                .task {
+                    guard let lastCreature = creatures.creaturesArray.last else { return }
+                    if creatures.creaturesArray[index].name == lastCreature.name && creatures.urlString.hasPrefix("http") {
+                        await creatures.getData()
+                    }
                 }
             }
             .listStyle(.plain)
             .navigationTitle("Pokemon")
+            .toolbar {
+                ToolbarItem(placement: .status) {
+                    Text("\(creatures.creaturesArray.count) of \(creatures.count)")
+                }
+            }
         }
         .task {
             await creatures.getData()
