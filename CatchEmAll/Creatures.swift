68efb60871f96f7f4a7d7e1b357faab1d16eb02d
nbmcguire <57973784+nbmcguire@users.noreply.github.com>
2025-03-18 00:13:05 -0400

    Changes from 6.7

--- CatchEmAll/Creatures.swift
+++ CatchEmAll/Creatures.swift
@@ -17,25 +17,31 @@ class Creatures {
     var urlString = "https://pokeapi.co/api/v2/pokemon/"
     var count = 0
     var creaturesArray: [Creature] = []
+    var isLoading = false
     func getData() async {
         print("We are accessing the url \(urlString)")
+        isLoading = true
         guard let url = URL(string: urlString) else {
             print("ERROR: Could not create a url from \(urlString)")
+            isLoading = false
             return
         }
         do {
             let (data, _) = try await URLSession.shared.data(from: url)
             guard let returned = try? JSONDecoder().decode(Returned.self, from: data) else {
                 print("ERROR: Could not decode returned JSON data")
+                isLoading = false
                 return
             }
             Task { @MainActor in
                 self.count = returned.count
                 self.urlString = returned.next ?? ""
                 self.creaturesArray = self.creaturesArray + returned.results
+                isLoading = false
             }
         } catch {
             print("ERROR: Could not get data from \(urlString)")
+            isLoading = false
         }
     }
 }
--- CatchEmAll/CreaturesListView.swift
+++ CatchEmAll/CreaturesListView.swift
@@ -11,27 +11,35 @@ struct CreaturesListView: View {
     @State var creatures = Creatures()
     var body: some View {
         NavigationStack {
-            List(0..<creatures.creaturesArray.count, id: \.self) { index in
-                LazyVStack {
-                    NavigationLink {
-                        DetailView(creature: creatures.creaturesArray[index])
-                    } label: {
-                        Text("\(index+1). \(creatures.creaturesArray[index].name.capitalized)")
-                            .font(.title2)
+            ZStack {
+                List(0..<creatures.creaturesArray.count, id: \.self) { index in
+                    LazyVStack {
+                        NavigationLink {
+                            DetailView(creature: creatures.creaturesArray[index])
+                        } label: {
+                            Text("\(index+1). \(creatures.creaturesArray[index].name.capitalized)")
+                                .font(.title2)
+                        }
+                    }
+                    .task {
+                        guard let lastCreature = creatures.creaturesArray.last else { return }
+                        if creatures.creaturesArray[index].name == lastCreature.name && creatures.urlString.hasPrefix("http") {
+                            await creatures.getData()
+                        }
                     }
                 }
-                .task {
-                    guard let lastCreature = creatures.creaturesArray.last else { return }
-                    if creatures.creaturesArray[index].name == lastCreature.name && creatures.urlString.hasPrefix("http") {
-                        await creatures.getData()
+                .listStyle(.plain)
+                .navigationTitle("Pokemon")
+                .toolbar {
+                    ToolbarItem(placement: .status) {
+                        Text("\(creatures.creaturesArray.count) of \(creatures.count)")
                     }
                 }
-            }
-            .listStyle(.plain)
-            .navigationTitle("Pokemon")
-            .toolbar {
-                ToolbarItem(placement: .status) {
-                    Text("\(creatures.creaturesArray.count) of \(creatures.count)")
+                
+                if creatures.isLoading {
+                    ProgressView()
+                        .tint(.red)
+                        .scaleEffect(4)
                 }
             }
         }
