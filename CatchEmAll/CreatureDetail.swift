e1aa2667fe7bdf071090acad93b6171848a25089
nbmcguire <57973784+nbmcguire@users.noreply.github.com>
2025-03-24 14:01:09 -0400

    Changes from 6.9

--- CatchEmAll/CreatureDetail.swift
+++ CatchEmAll/CreatureDetail.swift
@@ -16,8 +16,20 @@ class CreatureDetail {
     }
     
     struct Sprite: Codable {
-        var front_default: String
+        var other: Other
     }
+    
+    struct Other: Codable {
+        var officialArtwork: OfficialArtwork
+        enum CodingKeys: String, CodingKey {
+            case officialArtwork = "official-artwork"
+        }
+    }
+    
+    struct OfficialArtwork: Codable {
+        var front_default: String?
+    }
+    
     var urlString = ""
     var height = 0.0
     var weight = 0.0
@@ -37,7 +49,7 @@ class CreatureDetail {
             }
             self.height = returned.height
             self.weight = returned.weight
-            self.imageURL = returned.sprites.front_default
+            self.imageURL = returned.sprites.other.officialArtwork.front_default ?? "n/a"
         } catch {
             print("ERROR: Could not get data from \(urlString)")
         }
--- CatchEmAll/DetailView.swift
+++ CatchEmAll/DetailView.swift
@@ -24,23 +24,7 @@ struct DetailView: View {
                 .padding(.bottom)
             
             HStack {
-                AsyncImage(url: URL(string: creatureDetail.imageURL)) { image in
-                    image
-                        .resizable()
-                        .scaledToFit()
-                        .background(.white)
-                        .clipShape(RoundedRectangle(cornerRadius: 16))
-                        .shadow(radius: 8, x: 5, y: 5)
-                        .overlay {
-                            RoundedRectangle(cornerRadius: 16)
-                                .stroke(.gray.opacity(0.5), lineWidth: 1)
-                        }
-                } placeholder: {
-                    RoundedRectangle(cornerRadius: 10)
-                        .foregroundStyle(.clear)
-                }
-                .frame(width: 96, height: 96)
-                .padding(.trailing)
+                creatureImage
                 
                 VStack(alignment: .leading) {
                     HStack(alignment: .top) {
@@ -71,6 +55,42 @@ struct DetailView: View {
             creatureDetail.urlString = creature.url
             await creatureDetail.getData()
         }
+    }
+}
+
+extension DetailView {
+    var creatureImage: some View {
+        AsyncImage(url: URL(string: creatureDetail.imageURL)) { phase in
+            if let image = phase.image {
+                image
+                    .resizable()
+                    .scaledToFit()
+                    .background(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 16))
+                    .shadow(radius: 8, x: 5, y: 5)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 16)
+                            .stroke(.gray.opacity(0.5), lineWidth: 1)
+                    }
+            } else if phase.image != nil {
+                Image(systemName: "questionmark.square.dashed")
+                    .resizable()
+                    .scaledToFit()
+                    .background(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 16))
+                    .shadow(radius: 8, x: 5, y: 5)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 16)
+                            .stroke(.gray.opacity(0.5), lineWidth: 1)
+                    }
+            } else {
+                ProgressView()
+                    .tint(.red)
+                    .scaleEffect(4)
+            }
+        }
+        .frame(width: 96, height: 96)
+        .padding(.trailing)
     }
 }
 
